7908757f001b4a429ed70f29df870a1ab28629ea
ema <123239459+asceius@users.noreply.github.com>
2024-03-31 01:27:17 -0500

    Update English.lua

--- Language Bank/English.lua
+++ Language Bank/English.lua
@@ -1,11 +1,3 @@
---[[
-	Notes:
-	
-	- A code editor with syntax highlighting (Visual Code Studio, Notepad++, really anything with lua support) is recommended for easier readability.
-	- Any value in a non-English file can be safely deleted, the game will fall back to the English text if a requested index can't be found.
-	- Stand name and move name translations aren't super important unless the language uses a different alphabet that can't be read with the English alphabet.
-]]
-
 return {
 	-- // Settings Menu
 
