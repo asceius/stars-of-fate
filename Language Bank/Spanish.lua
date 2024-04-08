9bed1f799c13a72e82ce88c90aa524e9c349ddff
ema <123239459+asceius@users.noreply.github.com>
2024-04-08 12:34:13 -0500

    Create Spanish.lua

---
+++ Language Bank/Spanish.lua
@@ -0,0 +1,113 @@
+return {
+	-- // Settings Menu
+
+	settings_submenu_title = "OPCIONES";
+	settings_submenu_sounds = "Audio";
+	settings_submenu_graphics = "Gráficos";
+	settings_submenu_controls = "Controles";
+	settings_submenu_misc = "Etc.";
+
+	settings_language_self = "Español";
+
+	settings_options_on = "Activado";
+	settings_options_off = "Desactivado";
+
+	settings_audio_voice = "Voces";
+	settings_audio_effects = "Efectos";
+	settings_audio_music = "Música";
+	settings_audio_hitsounds = "Sonidos de Ataques";
+
+	settings_controls_input_buffering = "Input Buffering";
+	settings_controls_input_buffering_time = "Tiempo de Buffer";
+	settings_controls_aim_type = "Tipo de Puntería";
+	settings_controls_keybinds = "Combinaciones de Teclas";
+	settings_controls_keybinds_profile = "Perfil de Combinaciones de Teclas";
+	settings_controls_mobile_input_method = "Método de Entrada (Mobile)";
+	settings_controls_target = "Mantener Presionado para Seleccionar";
+	settings_controls_camera_smoothness = "Fluidez de Cámara";
+
+	settings_effects_hiteffect = "Efectos de Ataques";
+	settings_effects_barrage = "Efectos de Barrages";
+	settings_effects_wind = "Efecto de Viento";
+	settings_effects_timestop = "Efecto de Timestop";
+	settings_effects_projectile = "Efectos de Escombros";
+
+	settings_graphics_blur = "Desenfoque";
+	settings_graphics_bloom = "Brillo";
+	settings_graphics_shadows = "Sombras";
+	settings_graphics_distanceblur = "Desenfoque a Distancia";
+	settings_graphics_camshake = "Vibración de Cámara";
+	settings_graphics_camshake_intensity = "Intensidad de Vibración De Cámara";
+	settings_graphics_rays = "Rayos Solares";
+	settings_graphics_effects = "Efectos Gráficos";
+	settings_graphics_motionblur = "Desenfoque de Movimiento";
+	settings_graphics_alwaysday = "Siempre de Día";
+	settings_graphics_dynamicfov = "FOV Dinámico";
+	settings_graphics_impactframes = "Impact Frames";
+	settings_graphics_performance = "Modo de Rendimiento";
+	settings_graphics_camweight = "Peso de Cámara";
+
+	settings_misc_age = "Tiempo del Servidor";
+	settings_misc_combo = "Indicador de Combo";
+	settings_misc_lowhp = "FX de HP Baja";
+	settings_misc_debug = "GUI de Debug";
+	settings_misc_tips = "Tips Aleatorios";
+	settings_misc_hitbox = "Mostrar Hitboxes";
+	settings_misc_fpswarning = "Advertencia de FPS";
+	settings_misc_cutscene = "Saltar Cinemáticas";
+	settings_misc_stun = "Indicador de Stun";
+	settings_misc_gang = "Mostrar Roles de Gang";
+	settings_misc_snapshot = "Snapshots";
+	settings_misc_language = "Lenguaje";
+
+
+	-- // Tips
+
+	--[[
+		[key:Button] for the message to use the KeyCode Name the player has set
+		Example: "Press [key:Barrage] to barrage" would turn into "Press E to barrage"
+			
+		Button Names: Barrage, Block, Dodge, Heavy, Kick, Mouse, Movement, ProjectileA, ProjectileB, Run, SpecialA, SpecialB, SpecialC, Summon, Taunt
+	--]]
+
+	tips_tip1 = "Puedes presionar [key:Dodge] mientras estás en el piso, otorgando i-frames durante la animación de recuperación.";
+	tips_tip2 = "Asegúrate de unirte al servidor de la comunidad para ver más sneaks y actualizaciones. También estamos abiertos a sugerencias y reportes de bugs.";
+	tips_tip3 = "Rogar por habilidades de Admin no te dará habilidades de Admin.";
+	tips_tip4 = "Puedes mantener presionado [key:Heavy] para cargar tu Heavy Punch. Un Heavy Punch completamente cargado rompe bloqueos y no puede ser bloqueado perfectamente.";
+	tips_tip5 = "Tus opciones son altamente personalizables. Siéntete libre de personalizarlas a tu gusto.";
+	tips_tip6 = "Puede que tus ataques tengan cooldowns cortos, pero no andes alrededor spammeando el mismo ataque. Perderá daño, knockback, e incluso puede perder stun.";
+	tips_tip7 = "El sistema de Input Buffer te permite colar ataques antes de que tu ataque actual termine. Asegúrate de utilizar esto a tu ventaja en combos.";
+	tips_tip8 = "Recibes mas daño cuando tu estamina está baja, intenta no correr mucho.";
+	tips_tip9 = "No hay tal cosa como RKers en un juego Sandbox PvP.";
+	tips_tip10 = "Los proyectiles obedecen las leyes de la gravedad, ¡así que asegúrate de apuntar respectivamente!";
+	tips_tip11 = "Asegúrate de explorar el mapa, ¡puede que encuentres cosas geniales tiradas alrededor!";
+	tips_tip12 = "No te olvides de revisar !cmds, los dueños de servidores privados tienen una lista enorme con la cual liar.";
+	tips_tip13 = "Correr de batallas te pone en más riesgo que afrontar a tu oponente directamente. ¡Jamás lo pierdas de vista!";
+	tips_tip14 = "Puedes bloquear ciertos ataques perfectamente presionando [key:Block] justo antes del impacto, ortorgando invencibilidad.";
+	tips_tip15 = "Esquivar sin moverte te dará i-frames completamente, a pesar de tu estamina.";
+	tips_tip16 = "Debajo de 30% HP con Crazy Diamond, comenzará un temporizador para activar el Rage Mode, pero puedes presionar [key:SpecialC] para activarlo antes.";
+	tips_tip17 = "Como un no-usuario de stand, puedes dañar stands que estén actualmente activos en un ataque. Esto también aplica a usuarios de stand con ataques de personaje.";
+	tips_tip18 = "Como un usuario de stand, puedes comenzar un ataque de stand mientras utilizas un ataque de personaje. Una aplicación común sería S.Off-[key:Mouse] x3 > [key:Summon] > [key:Barrage]";
+	tips_tip19 = "S.Off y S.On es terminología común en la comunidad para referirse a Stand-On y Stand-Off respectivamente.";
+	tips_tip20 = "Parar el tiempo es muy costoso. Asegúrate de tener suficiente Ultimate para parar el tiempo efectivamente.";
+	tips_tip21 = "El ataque Star Launcher de Star Platinum daña a todos los objectivos en su camino. Asegurate de apuntar a grupos de jugadores para maximizar la Ultimate obtenida!";
+
+	tips_rker = "Recuerda, matar al azar no existe en un juego de pelea como este.";
+
+
+	-- // System Messages
+
+	system_voice_system = "[SISTEMA]";
+	system_voice_console = "[CONSOLA]";
+
+	system_settings_snapshot_warning = "Las opciones de Snapshot son experimentales y no se guardarán a través de servidores.";
+
+	system_maploader_error_unregistered = "¡El mapa seleccionado no está registrado! Puede que sufras errores.";
+	system_maploader_error_outdated = "¡El mapa seleccionado es viejo! Puede que sufras errores.";
+	system_maploader_error_invalid = "¡El mapa seleccionado no tiene una etiqueta de versión válida! Puede que sufras errores";
+
+
+	-- // Generic / Commonly Shared Moves
+
+	moves_generic_dodge = "Esquivar";
+}
