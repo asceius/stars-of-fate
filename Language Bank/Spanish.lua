4b0198ff7caf1f02fdd496ddd90863e663c55723
emma <123239459+asceius@users.noreply.github.com>
2025-09-08 19:48:29 -0500

    yawnem yawnsum

--- Language Bank/Spanish.lua
+++ Language Bank/Spanish.lua
@@ -107,8 +107,6 @@ return {
 
 	-- // Guide
 
-	guide_loremipsum_short = "Lorem ipsum";
-	guide_loremipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 	guide_rules = "Reglas del Juego";
 
 	guide_index_tutorial_button = "Tutorial";
